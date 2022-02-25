9803987b771b7d7018ba3b88aa7d4d1baaa20139
Kosztolánczi Dominik <kosztolanczi.dominik@hotmail.com>
2022-02-25 20:31:33 +0100

    Initial commit

---
+++ OnlabCryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlabCryptoAppApp.swift
+//  OnlabCryptoApp
+//
+//  Created by Kosztolánczi Dominik on 2022. 02. 25..
+//
+
+import SwiftUI
+
+@main
+struct OnlabCryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
