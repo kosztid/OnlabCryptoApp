549afb096939f0f8ad02ffb1a103457caaab6689
Kosztolánczi Dominik <kosztolanczi.dominik@hotmail.com>
2022-10-06 15:34:46 +0200

    fix swiflint warning

--- Services/SingleStockDownloader.swift
+++ Services/SingleStockDownloader.swift
@@ -12,17 +12,13 @@ class SingleStockDownloader {
     var stockSub: AnyCancellable?
     @Published var stock = Stock(ticker: "Loading...", queryCount: 1, resultsCount: 1, adjusted: false, results: [], status: "Loading...", requestID: "Loading...", count: 1)
 
-//    init(symbol: String) {
-//        loadSingleStock(symbol: symbol)
-//    }
-
     func loadSingleStock(symbol: String) {
         print("lefutott")
         let urlString = "https://api.polygon.io/v2/aggs/ticker/\(symbol.uppercased())/range/1/day/2022-09-14/2022-09-30?adjusted=true&sort=asc&limit=120&apiKey=EYcBp6VoRXW0iyk_ch7sy3NgpEbAfXqs"
 
         let url = URL(string: urlString)
         if let url = url {
-            let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            let task = URLSession.shared.dataTask(with: url) { data, _, error in
                 if let error = error {
                     print("error getting quote: \(error)")
                     return
