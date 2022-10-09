1e4aa5923c2622587634227c6d8c55c519f9555e
Kosztolánczi Dominik <kosztolanczi.dominik@hotmail.com>
2022-10-09 11:03:22 +0200

    add a popup with numberpad textfield

--- Extensions/AlertWithTextfield.swift
+++ Extensions/AlertWithTextfield.swift
@@ -18,6 +18,25 @@ extension View {
         })
         rootController().present(alert, animated: true, completion: nil)
     }
+
+    func alertWithTfNumpad(title: String, message: String, hintText: String, primaryTitle: String, secTitle: String, primaryAction: @escaping (String) -> Void, secondaryAction: @escaping () -> Void) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addTextField { field in
+            field.placeholder = hintText
+            field.keyboardType = .numberPad
+        }
+        alert.addAction(.init(title: secTitle, style: .cancel) { _ in
+            secondaryAction()
+        })
+        alert.addAction(.init(title: primaryTitle, style: .default) { _ in
+            if let text = alert.textFields?[0].text {
+                primaryAction(text)
+            } else {
+                primaryAction("")
+            }
+        })
+        rootController().present(alert, animated: true, completion: nil)
+    }
     func rootController() -> UIViewController {
         guard let screen = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
             return .init()
