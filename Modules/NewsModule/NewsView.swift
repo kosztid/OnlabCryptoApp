41d87953f74d3549dbb73bca6cc817690daacba4
Kosztolánczi Dominik <kosztolanczi.dominik@hotmail.com>
2022-10-15 20:34:34 +0200

    refactor and fix bug in newsview

--- Modules/NewsModule/NewsView.swift
+++ Modules/NewsModule/NewsView.swift
@@ -10,36 +10,26 @@ import SwiftUI
 struct NewsView: View {
     @ObservedObject var presenter: NewsPresenter
     var body: some View {
-        ZStack{
-            Color.theme.backgroundcolor
-                .ignoresSafeArea()
-            
-            List{
-                //Array(Set(presenter.news.articles!))
-                ForEach(presenter.news.articles!, id: \.self) { article in
-                    ZStack{
-                        Color.theme.backgroundcolor
-                                .ignoresSafeArea()
-                        NewsListItem(presenter: presenter, article: article)
-                            .frame(height: 60)
-                        self.presenter.linkBuilder(for: article) {
-                            EmptyView()
-                        }.buttonStyle(PlainButtonStyle())
-                    }
+        List {
+            // Array(Set(presenter.news.articles!))
+            ForEach(presenter.news.articles!, id: \.self) { article in
+                ZStack {
+                    Color.theme.backgroundcolor
+                        .ignoresSafeArea()
+                    NewsListItem(presenter: presenter, article: article)
+                        .frame(height: 60)
+                    self.presenter.linkBuilder(for: article) {
+                        EmptyView()
+                    }.buttonStyle(PlainButtonStyle())
                 }
-                .listRowSeparatorTint(Color.theme.backgroundsecondary)
-                .listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
             }
-            .navigationBarTitleDisplayMode(.inline)
-            .listStyle(PlainListStyle())
+            .listRowSeparatorTint(Color.theme.backgroundsecondary)
+            .listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
         }
-        
+
+        .background(Color.theme.backgroundcolor)
+        .scrollContentBackground(.hidden)
+        .navigationBarTitleDisplayMode(.inline)
+        .listStyle(PlainListStyle())
     }
 }
-/*
-struct NewsView_Previews: PreviewProvider {
-    static var previews: some View {
-        NewsView()
-    }
-}
-*/
