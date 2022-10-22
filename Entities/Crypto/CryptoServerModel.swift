b8f771def385ce7a8a0f00b498816a19603cbd1e
Kosztolánczi Dominik <kosztolanczi.dominik@hotmail.com>
2022-10-22 12:52:14 +0200

    add equatable protocol to cryptoservermodel

--- Entities/Crypto/CryptoServerModel.swift
+++ Entities/Crypto/CryptoServerModel.swift
@@ -1,10 +1,14 @@
 import Foundation
 
-struct CryptoServerModel: Codable {
+struct CryptoServerModel: Codable, Equatable {
     var id: Int
     var coinid: String
     var count: Double
     var buytotal: Double?
+
+    static func == (lhs: CryptoServerModel, rhs: CryptoServerModel) -> Bool {
+        return lhs.count == rhs.count
+    }
 }
 
 struct StockServerModel: Identifiable, Codable {
